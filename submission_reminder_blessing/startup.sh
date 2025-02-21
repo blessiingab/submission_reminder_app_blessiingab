cebd49fd2fbaace8257eb3ce0ea3649291dabac4
blessiingab <b.ingabire1@alustudent.com>
2025-02-21 12:55:20 -0800

    repair

--- submission_reminder_blessing/startup.sh
+++ submission_reminder_blessing/startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Check if the main directory exists
-if [ ! -d "submission_reminder_owen" ]; then
-    echo "Error: Directory submission_reminder_owen does not exist!"
+if [ ! -d "submission_reminder_blessing" ]; then
+    echo "Error: Directory submission_reminder_blessing does not exist!"
     exit 1
 fi
 
 # Navigate to the main directory
-cd "submission_reminder_owen" || exit 1
+cd "submission_reminder_blessing" || exit 1
 
 # Run the reminder script
 echo "Starting reminder application..."
